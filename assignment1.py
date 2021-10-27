da45b244a3724f924638fa19013c51d5713c56cd
Bryan Keane <20089166@mail.wit.ie>
2021-10-27 12:56:54 +0100

    Added SNS notifcation changes. A new key pair is now created if the specified key doesn't exist. Also added more specified exception handling.

--- assignment1.py
+++ assignment1.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 # Setup
-import os, sys, boto3, subprocess, webbrowser, random, string
+import os, sys, boto3, subprocess, webbrowser, random, string, time, stat
 from operator import itemgetter
 
 def fetch_latest_ami():
@@ -14,56 +14,61 @@ def fetch_latest_ami():
         image_details = sorted(
             response['Images'],
             key=itemgetter('CreationDate'),
+            # Sorts by newest to the end of the list so reverse so newest is at index 0
             reverse=True
         )
-        ami = image_details[0]['ImageId']
-        print('Most recent AMI found:', ami)
-        return ami
+        
+        newest_ami = image_details[0]['ImageId']
+        print('Most recent AMI found:', newest_ami)
+        return newest_ami
 
     except Exception as e:
         print('An error occurred while fetching the most recent Amazon Linux 2 AMI.')
         print(e)
         
-def instance_setup():
+def instance_setup(key_name):
     ec2_client = boto3.client('ec2')
     ec2 = boto3.resource('ec2')
-    newInstance = launch_instance()
+    new_instance = launch_instance(key_name)
 
     try:
+        new_instance[0].wait_until_running()
+        print('Instance running ~(˘▾˘~)')
+        
         # Reloads the instance and assigns the intance's ip to ec2_ip
-        newInstance[0].reload()
-        ec2_ip = newInstance[0].public_ip_address
+        new_instance[0].reload()
+        ec2_ip = new_instance[0].public_ip_address
 
         # Waits until web server is running to copy monitor.sh over and ssh in
         print('Please wait while the web server launches...')
+
         waiter = ec2_client.get_waiter('instance_status_ok')
-        waiter.wait(InstanceIds=[newInstance[0].instance_id])
-        subprocess.run("scp -o StrictHostKeyChecking=no -i bryankeanekeypair.pem monitor.sh ec2-user@" + ec2_ip + ":.", shell=True)
-        subprocess.run("ssh -o StrictHostKeyChecking=no -i bryankeanekeypair.pem ec2-user@" + ec2_ip + " 'chmod 700 monitor.sh'", shell=True)
-        subprocess.run("ssh -o StrictHostKeyChecking=no -i bryankeanekeypair.pem ec2-user@" + ec2_ip + " ' ./monitor.sh'", shell=True)
+        waiter.wait(InstanceIds=[new_instance[0].instance_id])
+        subprocess.run("scp -o StrictHostKeyChecking=no -i {}.pem monitor.sh ec2-user@".format(key_name) + ec2_ip + ":.", shell=True)
+        subprocess.run("ssh -o StrictHostKeyChecking=no -i {}.pem ec2-user@".format(key_name) + ec2_ip + " 'chmod 700 monitor.sh'", shell=True)
+        subprocess.run("ssh -o StrictHostKeyChecking=no -i {}.pem ec2-user@".format(key_name) + ec2_ip + " ' ./monitor.sh'", shell=True)
 
         # Launches web browser with public ip opened
         print('Opening webpage...')
         webbrowser.open_new_tab(ec2_ip)
 
     except Exception as e:
-        print('An error occurred while setting up monitoring.')#
+        print('An error occurred while setting up monitoring.')
         print(e)
 
-def launch_instance():
+def launch_instance(key_name):
     ec2_client = boto3.client('ec2')
     ec2 = boto3.resource('ec2')
-
     try:
         print('Starting Instance...')
-        # Creates a list: newInstance, containing the newly created instance
-        newInstance = ec2.create_instances(
+        # Creates a list: new_instance, containing the newly created instance
+        return ec2.create_instances(
             # Amazon Linux 2 AMI
             ImageId = fetch_latest_ami(), 
             MinCount = 1,
             MaxCount = 1,
             InstanceType = 't2.nano',
-            KeyName = 'bryankeanekeypair',
+            KeyName = key_name,
             # Group allows SSH access and HTTP traffic
             SecurityGroupIds = [
                 'sg-0feb83198fdf59512'       
@@ -98,16 +103,29 @@ def launch_instance():
                 }
             ]
         )
-        # Send text message notification using SNS to remind me to terminate the EC2 Instance
-        send_sns_text_msg('You just launched an instance, dont forget to terminate it when youre finished.')
-
-        newInstance[0].wait_until_running()
-        print('Instance running ~(˘▾˘~)')
-        return newInstance
 
     except Exception as e:
-        print('An error occurred during EC2 Instance creation.')
-        print(e)
+        if e.response['Error']['Code'] == 'InvalidKeyPair.NotFound':
+            print('The used key pair can not be found, creating a new one.')
+            print()
+            instance_setup(create_key_pair(key_name))
+
+        else:
+            print('An unexpected error occurred during EC2 Instance creation.')
+            print(e)
+
+def create_key_pair(name):
+    client = boto3.client("ec2")
+
+    print('New key pair:', name + ', successfully created.')
+    key_pair = client.create_key_pair(KeyName=name)
+    # create_key_pair() uploads public key to AWS, so now create private key to download locally
+    private_key = key_pair["KeyMaterial"]
+
+    # Write private key to file with 400 permissions
+    with os.fdopen(os.open("./{}.pem".format(name), os.O_WRONLY | os.O_CREAT, 0o400), "w+") as handle:
+        handle.write(private_key)
+    return name
 
 def create_bucket(bucket_name):
     s3 = boto3.resource("s3")
@@ -126,21 +144,21 @@ def create_bucket(bucket_name):
 
         s3_website_conversion(bucket_name)
         populate_bucket(bucket_name)
-
-        # Send text message notification using SNS to remind me to terminate the S3 Bucket
-        #send_sns_text_msg('You just launched an S3 bucket, dont forget to terminate it when youre finished.')
-                
         print('Loading website...')
+    
+        # Reloads bucket website to make sure all changes are up to date, then opens browser automatically for user
+        s3.BucketWebsite(bucket_name).reload()
         webbrowser.open_new_tab('https://{}.s3.eu-west-1.amazonaws.com/index.html'.format(bucket_name))
     
     except Exception as e:
-
-        print('The bucket name already exists')
         if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            print('The bucket name already exists')
             txt = input('Randomise the bucket name? (y/n)')
 
             if txt=='y' or txt == 'Y':
+                # Joins the bucket name string with a random selection of lowercase characters and creates a new bucket with that name
                 bucket_name2 = bucket_name.join(random.choices(string.ascii_lowercase + string.digits, k=2))
+                print('New bucket name:', bucket_name2)
                 create_bucket(bucket_name2)
 
             else: 
@@ -206,18 +224,20 @@ def sns_topic_setup(name):
     try:
         topic = sns.create_topic(Name=name)
         return topic
+
     except Exception as e:
         print('An error has occurred while setting up your topic.')
         print(e)
 
 def sns_sub_to_topic(topic, protocol, endpoint):
     try:
-        subscription=topic.subscribe(
-            Protocol=protocol,
-            Endpoint=endpoint,
-            ReturnSubscriptionArn=True
+        subscription = topic.subscribe(
+            Protocol = protocol,
+            Endpoint = endpoint,
+            ReturnSubscriptionArn = True
         )
         return subscription
+
     except Exception as e:
         print('An error has occurred while subscribing to your chosen topic.')
         print(e)
@@ -226,10 +246,11 @@ def publish_text_message(number, msg):
     sns = boto3.resource('sns')
     try:
         response = sns.meta.client.publish(
-            PhoneNumber=number,
-            Message=msg
+            PhoneNumber = number,
+            Message = msg
         )
         message_id=response['MessageId']
+
     except Exception as e:
         print('An error has occured while publishing your text message.')
         print(e)
@@ -237,12 +258,20 @@ def publish_text_message(number, msg):
 def send_sns_text_msg(msg):
     try:
         topic=sns_topic_setup('Assignment_Topic')
-        number = '+353851791723'
-        number_sub=sns_sub_to_topic(topic, 'sms', number)
+        number = '+353861727312'
+        number_sub = sns_sub_to_topic(topic, 'sms', number)
         publish_text_message(number, msg)
     
     except Exception as e:
         print('An error has occured while sending your text message.')
         print(e)
 
+print('INSTANCE SETUP')
+instance_setup('bkkeypair')
+send_sns_text_msg('You just launched an instance, dont forget to terminate it when youre finished.')
+
+print()
+
+print('BUCKET SETUP')
 create_bucket('keane-bryan-s3')
+send_sns_text_msg('You just launched an S3 bucket, dont forget to terminate it when youre finished.')
